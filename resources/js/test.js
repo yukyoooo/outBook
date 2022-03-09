63a0dbd0a564601e86304f9af2e19eb5892492bf
moriyama yukyo <moriyama-yukyo@alfos.jp>
2022-03-09 09:41:45 +0900

    update readme

---
+++ resources/js/test.js
@@ -0,0 +1,31 @@
+describe("statement", function (){
+    it("出力結果が正しいかチェック", function (){
+        const plays = {
+            'hamlet' : {'name': 'Hamlet', 'type': 'tragedy'},
+            'as-like' : {'name': 'As You Like It', 'type': 'comedy'},
+            'othello' : {'name': 'Othello', 'type': 'tragedy'},
+        };
+        const invoices =
+            {
+                'costomer': 'BigCo',
+                'performances': [
+                    {
+                        'playID': 'hamlet',
+                        'audience': 55,
+                    },
+                    {
+                        'playID': 'as-like',
+                        'audience': 35,
+                    },
+                    {
+                        'playID': 'othello',
+                        'audience': 40
+                    }
+                ]
+            };
+        const expected = 'Statement for BigCo\n    Hamlet: $650.00 (55 seats)\n    As You Like It: $580.00 (35 seats)\nOthello: $500.00 (40 seats)\nAmount owed is $1,730.00\nyou earned 47 credits';
+        assert.equal(statement(invoices, plays), expected);
+    })
+})
+
+

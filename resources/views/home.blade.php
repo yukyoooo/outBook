d4e3e90c3e6c76858c33dd3f12a48c5ee6a7b835
moriyama yukyo <moriyama-yukyo@alfos.jp>
2022-03-10 14:03:51 +0900

    変数名の変更

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -72,19 +72,19 @@
                 return result;
 
                 function totalAmount(){
-                    let totalAmount = 0;
+                    let result = 0;
                     for(let perf of invoice.performances){
-                        totalAmount += amountFor(perf);
+                        result += amountFor(perf);
                     }
-                    return totalAmount;
+                    return result;
                 }
 
                 function totalVolumeCredits() {
-                    let volumeCredits = 0;
+                    let result = 0;
                     for(let perf of invoice.performances){
-                        volumeCredits += volumeCreditsFor(perf);
+                        result += volumeCreditsFor(perf);
                     }
-                    return volumeCredits;
+                    return result;
                 }
 
                 function volumeCreditsFor(aPerformance){

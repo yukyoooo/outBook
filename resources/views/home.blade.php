3fb04f670921d8a2387fb0877b3ca3eccf49e450
moriyama yukyo <moriyama-yukyo@alfos.jp>
2022-03-10 14:51:27 +0900

    suctomerを中間データへ移動

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -62,11 +62,12 @@
 
             function statement (invoice, plays){
                 const statementData = {};
+                statementData.customer = invoice.customer;
                 return renderPlainText(statementData, invoice, plays);
             }
 
             function renderPlainText(data, invoice, plays){
-                let result = `Statement for ${invoice.costomer}\n`;
+                let result = `Statement for ${data.customer}\n`;
                 for (let perf of invoice.performances) {
                     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
                 }

84cfa168c4595298054ee927fc27d8d7204dd91f
moriyama yukyo <moriyama-yukyo@alfos.jp>
2022-03-10 14:12:45 +0900

    関数の抽出

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -61,12 +61,14 @@
                 };
 
             function statement (invoice, plays){
+                return renderPlainText(invoice, plays);
+            }
+
+            function renderPlainText(invoice, plays){
                 let result = `Statement for ${invoice.costomer}\n`;
                 for (let perf of invoice.performances) {
-                    //注文の内訳を出力
                     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
                 }
-
                 result += `Amount owed is ${usd(totalAmount())}\n`;
                 result += `you earned ${totalVolumeCredits()} credits\n`;
                 return result;
@@ -78,7 +80,6 @@
                     }
                     return result;
                 }
-
                 function totalVolumeCredits() {
                     let result = 0;
                     for(let perf of invoice.performances){
@@ -86,24 +87,20 @@
                     }
                     return result;
                 }
-
                 function volumeCreditsFor(aPerformance){
                     let result = 0;
                     result += Math.max(aPerformance.audience - 30, 0);
                     if("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
                     return result;
                 }
-
                 function usd(aNumber){
                     return new Intl.NumberFormat("en-US",
                         { style: "currency", currency: "USD",
                             minimumFractionDigits: 2 }).format(aNumber/100);
                 }
-
                 function playFor(aPerformance){
                     return plays[aPerformance.playID];
                 }
-
                 function amountFor(aPerformance){
                     let result = 0;
                     switch (playFor(aPerformance).type) {

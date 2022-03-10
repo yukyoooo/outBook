a9bd67b17f4366c452242e83d0f808b475acfdfb
moriyama yukyo <moriyama-yukyo@alfos.jp>
2022-03-10 15:06:46 +0900

    play,amountの関数の移動

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -68,50 +68,17 @@
 
                 function enrichPerformance(aPerformance){
                     const result = Object.assign({}, aPerformance);
+                    result.play = playFor(result);
+                    result.amount = amountFor(result);
                     return result;
                 }
-            }
 
-            function renderPlainText(data, plays){
-                let result = `Statement for ${data.customer}\n`;
-                for (let perf of data.performances) {
-                    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-                }
-                result += `Amount owed is ${usd(totalAmount())}\n`;
-                result += `you earned ${totalVolumeCredits()} credits\n`;
-                return result;
-
-                function totalAmount(){
-                    let result = 0;
-                    for(let perf of data.performances){
-                        result += amountFor(perf);
-                    }
-                    return result;
-                }
-                function totalVolumeCredits() {
-                    let result = 0;
-                    for(let perf of data.performances){
-                        result += volumeCreditsFor(perf);
-                    }
-                    return result;
-                }
-                function volumeCreditsFor(aPerformance){
-                    let result = 0;
-                    result += Math.max(aPerformance.audience - 30, 0);
-                    if("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
-                    return result;
-                }
-                function usd(aNumber){
-                    return new Intl.NumberFormat("en-US",
-                        { style: "currency", currency: "USD",
-                            minimumFractionDigits: 2 }).format(aNumber/100);
-                }
                 function playFor(aPerformance){
                     return plays[aPerformance.playID];
                 }
                 function amountFor(aPerformance){
                     let result = 0;
-                    switch (playFor(aPerformance).type) {
+                    switch (aPerformance.play.type) {
                         case "tragedy":
                             result = 40000;
                             if (aPerformance.audience > 30) {
@@ -126,10 +93,47 @@
                             result += 300 * aPerformance.audience;
                             break;
                         default:
-                            throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+                            throw new Error(`unknown type: ${aPerformance.play.type}`);
                     }
                     return result;
                 }
+            }
+
+            function renderPlainText(data, plays){
+                let result = `Statement for ${data.customer}\n`;
+                for (let perf of data.performances) {
+                    result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+                }
+                result += `Amount owed is ${usd(totalAmount())}\n`;
+                result += `you earned ${totalVolumeCredits()} credits\n`;
+                return result;
+
+                function totalAmount(){
+                    let result = 0;
+                    for(let perf of data.performances){
+                        result += perf.amount;
+                    }
+                    return result;
+                }
+                function totalVolumeCredits() {
+                    let result = 0;
+                    for(let perf of data.performances){
+                        result += volumeCreditsFor(perf);
+                    }
+                    return result;
+                }
+                function volumeCreditsFor(aPerformance){
+                    let result = 0;
+                    result += Math.max(aPerformance.audience - 30, 0);
+                    if("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+                    return result;
+                }
+                function usd(aNumber){
+                    return new Intl.NumberFormat("en-US",
+                        { style: "currency", currency: "USD",
+                            minimumFractionDigits: 2 }).format(aNumber/100);
+                }
+
             }
 
 

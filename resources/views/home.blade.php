b9583faedee7213e75859bcd320df5a07cce28bd
moriyama yukyo <moriyama-yukyo@alfos.jp>
2022-03-10 13:43:23 +0900

    ステートメントのスライド

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -62,13 +62,14 @@
 
             function statement (invoice, plays){
                 let totalAmount = 0;
-                let volumeCredits = 0;
                 let result = `Statement for ${invoice.costomer}\n`;
                 for (let perf of invoice.performances) {
                     //注文の内訳を出力
                     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
                     totalAmount += amountFor(perf);
                 }
+
+                let volumeCredits = 0;
                 for (let perf of invoice.performances) {
                     volumeCredits += volumeCreditsFor(perf);
                 }

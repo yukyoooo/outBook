39bb3c21c0ce228a21f385b0e670226513d9cbe4
moriyama yukyo <moriyama-yukyo@alfos.jp>
2022-03-13 09:25:58 +0900

    サブクラスによるタイプコードの置き換え ファクトリ関数によるコンストラクタの置き換え ポリモーフィズムによる条件記述の置き換え

--- resources/js/refactoring/createStatementData.js
+++ resources/js/refactoring/createStatementData.js
@@ -8,11 +8,11 @@ export default function createStatementData(invoice, plays){
     return result;
 
     function enrichPerformance(aPerformance){
-        const calcuraltor = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
-        result.play = calcuraltor.play;
-        result.amount = calcuraltor.amount;
-        result.volumeCredits = calcuraltor.volumeCredits;
+        result.play = calculator.play;
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
     function playFor(aPerformance){
@@ -26,6 +26,16 @@ export default function createStatementData(invoice, plays){
         return data.performances
             .reduce((total, p) => total + p.volumeCredits, 0);
     }
+
+}
+
+function createPerformanceCalculator(aPerformance, aPlay){
+    switch(aPlay.type){
+    case "tragedy":return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":return new ComedyCalculator(aPerformance, aPlay);
+    default:
+        throw new Error(`未知の演劇の種類：${aPlay.type}`);
+    }
 }
 
 class PerformanceCalculator{
@@ -35,31 +45,35 @@ class PerformanceCalculator{
     }
 
     get amount(){
-        let result = 0;
-        switch (this.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${this.play.type}`);
+        throw new Error(`サブクラスの責務`);
+    }
+
+    get volumeCredits(){
+        return Math.max(this.performance.audience - 30, 0);
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator{
+    get amount(){
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
         }
+        return result;
+    }
+}
+
+class ComedyCalculator extends PerformanceCalculator{
+    get amount(){
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
         return result;
     }
 
     get volumeCredits(){
-        let result = 0;
-        result += Math.max(this.performance.audience - 30, 0);
-        if("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
-        return result;
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
     }
 }
